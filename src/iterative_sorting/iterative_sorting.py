16ab8f1f92fbd59c0cff1542811b4edf760fe5c9
Pherpher089 <Christutor089@gmail.com>
2020-02-03 23:51:27 -0800

    finnished merge sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,30 +1,78 @@
-# TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
+# TO-DO: Complete the selection_sort() function below
+def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
-
-
-
+        # (hint, can do in 3 loc)
+        for j in range(cur_index, len(arr)):
+            if arr[smallest_index] > arr[j]:
+                smallest_index = j
         # TO-DO: swap
-
-
-
-
+        placeHolder = arr[smallest_index]
+        arr[smallest_index] = arr[cur_index]
+        arr[cur_index] = placeHolder
     return arr
 
+
+test_arr = [9, 4, 3, 7, 6, 8, 2, 5, 1]
+test_arr = selection_sort(test_arr)
+
+print("SELECTION SORT")
+for i in test_arr:
+    print(i)
 
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
+
+
+def bubble_sort(arr):
+    no_sort = False
+
+    while no_sort == False:
+        no_sort = True
+        for i in range(0, len(arr) - 1):
+            if arr[i] > arr[i + 1]:
+                place_holder = arr[i + 1]
+                arr[i+1] = arr[i]
+                arr[i] = place_holder
+                no_sort = False
 
     return arr
 
+
+test_arr = [9, 4, 1, 5, 5, 3, 7, 6, 8, 2, 5, 1]
+test_arr = bubble_sort(test_arr)
+
+print("BUBBLE SORT")
+for i in test_arr:
+    print(i)
 
 # STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
 
+
+def count_sort(arr, maximum=-1):
+    counter = [0] * (maximum + 1)
+
+    for i in range(0, len(arr) - 1):
+        counter[arr[i]] += 1
+
+    for i in range(0, maximum):
+        counter[i + 1] = counter[i] + counter[i + 1]
+
+    _arr = [0] * len(arr)
+
+    for i in range(0, len(arr)):
+        _arr[counter[arr[i]]] = arr[i]
+        counter[arr[i]] -= 1
+
+    arr = _arr
     return arr
+
+
+test_arr = [9, 4, 3, 7, 6, 8, 2, 5, 1]
+test_arr = count_sort(test_arr, 9)
+
+print("COUNT SORT")
+for i in test_arr:
+    print(i)
--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,26 +1,53 @@
-# TO-DO: complete the helpe function below to merge 2 sorted arrays
-def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
+# TO-DO: complete the help function below to merge 2 sorted arrays
+def merge(arrA, arrB):
+    elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
     # TO-DO
-    
+    # Still need to figure out what to do with
+    # a single element
+
+    for i in range(len(merged_arr)):
+        if(len(arrA) == 0):
+            merged_arr[i] = arrB[0]
+            arrB.pop(0)
+        elif(len(arrB) == 0):
+            merged_arr[i] = arrA[0]
+            arrA.pop(0)
+        elif(arrA[0] < arrB[0]):
+            merged_arr[i] = arrA[0]
+            arrA.pop(0)
+        elif(arrA[0] > arrB[0]):
+            merged_arr[i] = arrB[0]
+            arrB.pop(0)
+
     return merged_arr
 
-
 # TO-DO: implement the Merge Sort function below USING RECURSION
-def merge_sort( arr ):
+
+
+def merge_sort(arr):
     # TO-DO
+    if(len(arr) > 1):
+        m = len(arr)//2
+        arrA = merge_sort(arr[:m])
+        arrB = merge_sort(arr[m:])
+        arr = merge(arrA, arrB)
 
     return arr
 
 
+test_list = [r]
+print(merge_sort(test_list))
 # STRETCH: implement an in-place merge sort algorithm
+
+
 def merge_in_place(arr, start, mid, end):
     # TO-DO
 
     return arr
 
-def merge_sort_in_place(arr, l, r): 
+
+def merge_sort_in_place(arr, l, r):
     # TO-DO
 
     return arr
@@ -28,6 +55,6 @@ def merge_sort_in_place(arr, l, r):
 
 # STRETCH: implement the Timsort function below
 # hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
-def timsort( arr ):
+def timsort(arr):
 
     return arr
